e813f12906aff87a1683439213620db5c685fec0
martijnv <martijnvoordouw@gmail.com>
2024-02-03 15:52:55 +0100

    de rest toegevoegd

---
+++ .config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # change default greeting
+    function fish_greeting
+        #echo "\
+#⠀⠀⠀⠀⠀⠀⠀⢠⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢺⣷⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⢸⣿⡄⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡆⠀⠀⠀⠀⠀⠀⠀⣾⣿⠁⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠘⣿⣧⡀⠀⠀⠀⠀⠀⢠⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣷⡀⠀⠀⠀⠀⠀⣰⣿⡟⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣷⣄⡀⠀⢀⣴⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣦⣀⠀⣀⣴⣿⠟⠀⠀⢀⣀⣀⡀⠀⠀
+#⠀⢀⠴⠒⠉⠉⠉⠲⢄⠈⠻⢿⣿⣿⣿⡿⠟⠁⠀⠀⢠⡆⠀⠀⠀⠀⠀⠀⢰⣶⠀⠀⠀⠀⠙⠿⣿⣿⡿⠟⠋⣠⠔⠊⠁⠀⠀⠈⠲⡄
+#⢠⠃⠀⠀⠀⠀⠀⠀⠀⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⢠⣤⡀⢀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀⠀⠀⠀⠀⠀⠘
+#⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣷⣴⣿⢿⣿⣾⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⢀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⡰
+#⠈⠢⠀⠀⠀⠀⠀⣀⠔⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⢄⡀⠀⠀⠀⡠⠔⠁
+#" # | lolcat
+    end
+    # remove default vi mode prompt
+    function fish_mode_prompt; end
+    # Commands to run in interactive sessions can go here
+
+    fish_vi_key_bindings
+    #fish_default_key_bindings
+
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert line
+    # Set the replace mode cursor to an underscore
+    set fish_cursor_replace_one underscore
+
+    alias ll="eza --icons -la --group-directories-first"
+    alias drive="rclone mount --daemon --vfs-cache-mode full martijn-google-drive:/ ~/Documents/drive/"
+    set -gx EDITOR nvim
+
+    export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
+    export PATH="$PATH:$HOME/.config/emacs/bin"
+    #starship init fish | source
+    #should be at the eof
+    zoxide init fish | source
+end
+
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,56 @@
+function fish_prompt --description 'Write out the prompt'
+    #add blank line
+    echo ""
+
+    set -l last_status $status
+    set -l normal (set_color normal)
+    set -l status_color (set_color brgreen)
+    set -l cwd_color (set_color $fish_color_cwd)
+    set -l vcs_color (set_color brpurple)
+    set -l prompt_status ""
+
+    # Since we display the prompt on a new line allow the directory names to be longer.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
+    # Color the prompt differently when we're root
+    set -l suffix '❯'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set cwd_color (set_color $fish_color_cwd_root)
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $last_status -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    
+
+    # EDIT
+    switch $fish_bind_mode
+    case default
+        set_color --bold red
+        set mode_ind 'Normal '
+    case insert
+        set_color --bold green
+        set mode_ind 'Insert '
+    case replace
+        set_color --bold green
+        set mode_ind 'Replace'
+    case visual
+        set_color --bold brmagenta
+        set mode_ind 'Visual '
+    case '*'
+        echo $fish_bind_mode
+        set_color --bold red
+        set mode_ind '?'
+    end
+    echo -n -s $mode_ind $status_color $suffix ' ' $normal
+end
+
+
