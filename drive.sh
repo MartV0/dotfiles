0ed8f94d586d37a47009bfa7d0bcf02efd557eca
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-11-22 15:58:55 +0100

    startup script now waits for internet before doing rsync

--- drive.sh
+++ drive.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# wait for internet to be available before mounting
+
+while ! ping -c 1 -W 1 example.com; 
+do 
+    sleep 2 
+done
+
 rclone mount --daemon --vfs-cache-mode full google_drive:/ /home/martijn/Documents/drive
